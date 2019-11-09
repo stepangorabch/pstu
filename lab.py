bdac52a0ee9d993b1ba1144bd9afe2451d169e07
stepangorbach <stepan.gorbach@outlook.com>
2019-11-09 16:32:49 +0500

    добавлены файлы с лабораторок

--- lab.py
+++
@@ -1,3 +0,0 @@
-userInput = input("Hello friend, what is your name? ")
-if userInput == "Максим" or "Maxim" or "Max":
-    print("О, Максим, что тут делаешь? Я думал ты пошел нахуй")
---
+++ lab3.py
@@ -0,0 +1,26 @@
+#1. Написать программу вычисления суммы факториалов всех нечетных чисел от 1 до 9.
+#2. Дано четное число n > 2. Проверить для него гипотезу Гольдбаха: каждое четное представляется в виде суммы двух простых чисел.
+
+#задача 1
+def factorial(x):
+    factDef = 1
+    for i in range(1,x+1):
+        factDef = factDef*i
+    return(factDef)
+
+def SuperEpicMegaFunction(k):
+    sum = 0
+    for j in range(1, k+1):
+        if j%2 == 1:
+            sum += factorial(j)
+            print('Номер фаториала:', j, 'Сумма равна:', sum)
+    print('Сумма факториалов равна:', sum)
+
+userInput = int(input('\nВведите число: '))
+SuperEpicMegaFunction(userInput)
+
+#задача 2
+
+
+
+
