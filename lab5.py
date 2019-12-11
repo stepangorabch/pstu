4d1c23aea150335ad407a96a74001ce52de1fdaa
stepangorbach <stepan.gorbach@outlook.com>
2019-12-11 22:14:54 +0500

    lab 5  complete

--- lab5.py
+++ lab5.py
@@ -71,35 +71,77 @@ def matrix3(x, y):
         print('\n',numList)
 
 #matrix3(int(input("Введите число строк: ")),int(input("Введите число столбцов: ")))
-    
+
 
 def matrix4(x):
-    matrixList = [[] * x for i in range(x)]
-    iX = 0; iY = 0; n = 1; num = 1
+    matrixList = []
+    for i in range(x):
+        matrixList.append([0] * x)
+    
+    matrixList[0][0] = 1
+    #print(matrixList)
+    
+    idX = 0; idY = 0
+    n = 1; num = x-2
 
-    while n != x:
-        if n%2 == 0:
-            iX = 0
-            iY = 0
-            while iX != 0:
-                matrixList[iX][iY] = num
-                num += 1
-                iX += 1
-                if iY != 0:
-                    iY -= 1
+    for i in range((2*x)-1):
+        #print("\ni = ",i)
+        if idX == 0 and i < x-1:
+            #print("X")
+            #print("idY :", idY, "idX: ", idX)
+            idY += 1
+            n += 1
+            matrixList[idY][idX] = n
+            for k in range(i+1):
+                n += 1
+                idY -= 1
+                idX += 1
+                matrixList[idY][idX] = n
+        
+        elif idY == 0 and i < x-1:
+            #print("\nY")
+            #print("idY :", idY, "idX: ", idX)
+            idX += 1
+            n += 1
+            matrixList[idY][idX] = n
+            for k in range(i+1):
+                n += 1
+                idX -= 1
+                idY += 1
+                matrixList[idY][idX] = n
 
-        else:
-            iX = 0
-            iY = 0
-            while iY != 0:
-                matrixList[(iY-1)][iX] = num
-                num += 1
-                if iX != 0:
-                    iX -= 1
-                iY += 1
+        elif idX == (x-1) and i >= x:
+            #rint("\nX")
+            #print("idY :", idY, "idX: ", idX)
+            idY += 1
+            n += 1
+            matrixList[idY][idX] = n
+            for k in range(num):
+                n += 1
+                idX -= 1
+                idY += 1
+                matrixList[idY][idX] = n
+            num -= 1
 
-        n += 1
+        elif idY == (x-1) and i >= x:
+            #print("\nY")
+            #print("idY :", idY, "idX: ", idX)
+            idX += 1
+            n += 1
+            matrixList[idY][idX] = n
+            for k in range(num):
+                n += 1
+                idY -= 1
+                idX += 1
+                matrixList[idY][idX] = n
+            num -= 1
 
-    print(matrixList)
+                
 
-matrix4(5)
+    print("\n")
+    for i in range(len(matrixList[0])):
+        print(matrixList[i])
+
+
+for i in range(1, 11):
+    matrix4(i)
